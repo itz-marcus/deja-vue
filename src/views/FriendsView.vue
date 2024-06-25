3e0aad82a7291434695b64342ddd4f052c2df560
itz-marcus <marcusadonis5@gmail.com>
2024-06-25 13:01:48 +0200

    Friends finished

--- src/views/FriendsView.vue
+++ src/views/FriendsView.vue
@@ -1,13 +1,44 @@
-<template lang="">
+<template>
     <div>
-        
+        <div id="card">
+            <card-comp v-for ="friend in friendsData()" :key="friend">
+                <template #console>
+                    <h1>Name: {{friend.profile}}</h1>
+                    <p>Manufacturer: {{friend.firstName}}</p>
+                    <p>Release Year: {{friend.lastName}}</p>
+                    <p>Best selling game: {{friend.gender}}</p>
+                    <p>Price: R{{friend.contactDetails}}</p>
+                    <hr>
+                </template>
+            </card-comp>   
+        </div>
     </div>
 </template>
 <script>
+import CardComp from '@/components/CardComp.vue';
 export default {
-    
+    components:{
+        CardComp
+    },
+    methods:{
+        friendsData(){
+            return this.$store.state.friends
+        }
+    },
+    computed:{
+        getData(){
+            return this.$store.dispatch ("getData")
+        }
+    },
+    mounted(){
+        this.getData
+    }
 }
 </script>
-<style lang="">
-    
+<style scoped>
+    #card{
+        background-color: rgb(157, 112, 200);
+        margin: 2vh;
+        border-radius: 10px;
+    }
 </style>
